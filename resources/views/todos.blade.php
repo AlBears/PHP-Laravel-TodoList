6c1317efd8161144c7f03a9902023149c3d5b0ac
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-08-06 12:07:43 +0300

    insert todo

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts')
 
 @section('content')
+
+    <div class="row">
+      <div class="col-lg-6 col-lg-offset-3">
+        <form class="" action="/create/todo" method="post">
+          <input type="text"
+                  class="form-control input-lg"
+                  placeholder="Create a new todo"
+                  name="todo">
+          {{ csrf_field() }}
+        </form>
+
+      </div>
+    </div>
+
+    <hr>
     @foreach($todos as $todo)
 
     {{$todo->todo}}
