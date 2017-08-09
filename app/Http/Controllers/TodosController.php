3338c2187c5e005db8b602d6c39c7f3e68f90273
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-08-09 20:03:40 +0300

    flash messages with session

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Todo;
+use Session;
 
 class TodosController extends Controller
 {
@@ -19,6 +20,8 @@ class TodosController extends Controller
       $todo->todo = $request->todo;
       $todo->save();
 
+      Session::flash('success', 'Your todo was created');
+
       return redirect()->back();
     }
 
@@ -26,6 +29,8 @@ class TodosController extends Controller
     {
       $todo = Todo::find($id);
       $todo->delete();
+
+      Session::flash('success', 'Your todo was deleted');
 
       return redirect()->back();
     }
@@ -43,6 +48,8 @@ class TodosController extends Controller
       $todo->todo = $request->todo;
       $todo->save();
 
+      Session::flash('success', 'Your todo was updated');
+
       return redirect()->route('todos');
     }
 
@@ -51,6 +58,8 @@ class TodosController extends Controller
       $todo = Todo::find($id);
       $todo->completed = 1;
       $todo->save();
+
+      Session::flash('success', 'Your todo was set as completed');
 
       return redirect()->route('todos');
     }
